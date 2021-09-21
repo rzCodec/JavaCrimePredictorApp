d4ee78fe539235e42055f9472706c984b6439c01
Ronald <laironald52@gmail.com>
2021-09-21 19:37:53 +0200

    Update CrimeFrequency.java

--- CSC3A_MiniProject/src/Crime/CrimeFrequency.java
+++ CSC3A_MiniProject/src/Crime/CrimeFrequency.java
@@ -4,14 +4,11 @@ package Crime;
  * Created By Ronald Lai, 201433999
  */
 
-
-public class CrimeFrequency 
-{
+public class CrimeFrequency {
 	private String sCrimeType;
 	private int iFreq;
 
-	public CrimeFrequency(String sName, int iFreq) 
-	{
+	public CrimeFrequency(String sName, int iFreq) {
 		this.sCrimeType = sName;
 		this.iFreq = iFreq;
 	}
@@ -24,21 +21,16 @@ public class CrimeFrequency
 		this.sCrimeType = sCrimeType;
 	}
 
-	public int getiFreq() 
-	{
+	public int getiFreq() {
 		return iFreq;
 	}
-	public void setiFreq(int iFreq) 
-	{
+	
+	public void setiFreq(int iFreq) {
 		this.iFreq = iFreq;
 	}
+	
 	@Override
-	public String toString() 
-	{
+	public String toString() {
 		return "Crime Type :" + sCrimeType + " :: Frequency :" + iFreq + "\n";
 	}
- 
-	
-	
-
 }
