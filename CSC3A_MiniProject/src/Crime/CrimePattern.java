80adb5223eda3013ac5c0c6a2eedae9ec4adac1e
Ronald <laironald52@gmail.com>
2021-11-21 13:24:20 +0200

    Update CrimePattern.java

--- CSC3A_MiniProject/src/Crime/CrimePattern.java
+++ CSC3A_MiniProject/src/Crime/CrimePattern.java
@@ -1,26 +1,17 @@
 package Crime;
 import CustomDataStructures.*;
 
-/*
- * Created By Ronald Lai, 201433999
- */
-
-
-public class CrimePattern 
-{
+public class CrimePattern {
 	private int iTraffic;
 	private int iMorning;
 	private int iNight;
 	private int iSleep;
 	private int iDay;
 	
-	public CrimePattern()
-	{
-		
+	public CrimePattern(){
 	}
 
-	public boolean isOrganisedCrime(double stdDeviation, int iAvg)
-	{
+	public boolean isOrganisedCrime(double stdDeviation, int iAvg){
 		double dHalfAvg = iAvg / 2;
 		if(stdDeviation <= dHalfAvg) return true;
 		else return false;
@@ -31,11 +22,9 @@ public class CrimePattern
 	 * @param timeList
 	 * @return
 	 */
-	public int calcAvgTime(CustomList<Integer> timeList)
-	{
+	public int calcAvgTime(CustomList<Integer> timeList){
 		int iTotalTime = 0;
-		for(int time : timeList)
-		{
+		for(int time : timeList){
 			iTotalTime += time;
 		}
 		
@@ -52,8 +41,7 @@ public class CrimePattern
 	    double variance = 0;
 	    double stdDeviation = 0;
 	    
-	    for (int value : numList)
-	    {
+	    for (int value : numList){
 	        variance += ((value - iAvgTime) * (value - iAvgTime)) / numList.size();
 	    }
 	    
@@ -66,25 +54,20 @@ public class CrimePattern
 	 * @param iMaxTime
 	 * @return
 	 */
-	public String findCrimePattern(int iTime)
-	{
-		if((iTime >= 22) || (iTime <= 6)) 
-		{
+	public String findCrimePattern(int iTime){
+		if((iTime >= 22) || (iTime <= 6)) {
 			iSleep += 1;
 			return "\n Domestic Violence, Attempted Murder, Attempted Break-In, Kidnapping and Hi-jack crimes are high when people come home at night or while they are sleeping";
 		}
-		else if((iTime >= 6) && (iTime <= 9))
-		{
+		else if((iTime >= 6) && (iTime <= 9)){
 			iMorning += 1;
 			return "\n Assault, Hi-jack, Snatch & Grab is likely to occur in the morning hours";
 		}
-		else if((iTime >= 9) && (iTime <= 16))
-		{
+		else if((iTime >= 9) && (iTime <= 16)){
 			iDay += 1;
 			return "\n Robbery, Fraud, Attempted Break-In, Blackmail and Extortion crimes have a high chance of occurring during the day";
 		}
-		else if((iTime >= 16) && (iTime <= 22))
-		{
+		else if((iTime >= 16) && (iTime <= 22)){
 			iTraffic += 1;
 			return "\n Hi-jack, Assault, Snatch and Grab, Attempted Robbery and Kidnapping are likely due to rush hour traffic";
 		}
@@ -96,8 +79,7 @@ public class CrimePattern
 	 * @param timeList - A list of integer times that crimes have started at.
 	 * @return
 	 */
-	public int findCommonTime(CustomList<Integer> timeList)
-	{
+	public int findCommonTime(CustomList<Integer> timeList){
 		int iMax = timeList.get(0);
 		for(int time : timeList)
 		{
@@ -145,7 +127,4 @@ public class CrimePattern
 	public void setiDay(int iDay) {
 		this.iDay = iDay;
 	}
-	
-	
-
 }
