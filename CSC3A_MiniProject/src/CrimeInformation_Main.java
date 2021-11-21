cd50ee8f7599dcd67294b76592a4f661fdedc9fd
Ronald <laironald52@gmail.com>
2021-11-21 20:38:17 +0200

    Update CrimeInformation_Main.java

--- CSC3A_MiniProject/src/CrimeInformation_Main.java
+++ CSC3A_MiniProject/src/CrimeInformation_Main.java
@@ -1,23 +1,16 @@
 import java.util.*;
 import CustomDataStructures.CustomList;
 import java.util.concurrent.ThreadLocalRandom;
-
 import javax.swing.JFrame;
 import graphicInterface.*;
 import CustomDataStructures.*;
 
-/*
- * Created By Ronald Lai, 201433999
- */
-
 public class CrimeInformation_Main {
-
 	static int iRandomCities;
 	static int[] iRandomArySuburbs = null;
 	
-	public static void main(String[] args)
-	{	
-		CustomFrame Frame = new CustomFrame();
+	public static void main(String[] args) {	
+	    CustomFrame Frame = new CustomFrame();
 	    Frame.setVisible(true);
 	    Frame.pack();
 	    Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
