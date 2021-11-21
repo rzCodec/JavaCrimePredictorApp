04399dbdc8bd2d815814e558fc72082eb79c90eb
Ronald <laironald52@gmail.com>
2021-11-21 20:39:48 +0200

    Update CustomFrame.java

--- CSC3A_MiniProject/src/graphicInterface/CustomFrame.java
+++ CSC3A_MiniProject/src/graphicInterface/CustomFrame.java
@@ -5,26 +5,18 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-
 import javax.swing.*;
 import Crime.CrimeData;
 import CustomDataStructures.CrimeTree;
 
-/*
- * Created by Ronald Lai, 201433999
- */
-
-public class CustomFrame extends JFrame implements ActionListener
-{
+public class CustomFrame extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 3644110547115789312L;
-	
 	private JTextArea txtAreaDisplayStats;
 	private JScrollPane scrollPane;
 	private JButton btnGenerateStats;
 	private CrimeTree crimeTreeObj;
 	
-	public CustomFrame()
-	{
+	public CustomFrame() {
 		setTitle("Crime Patterns in 2017");
 		createCrimeTree();
 		JPanel BasePanel = new JPanel();
@@ -42,39 +34,32 @@ public class CustomFrame extends JFrame implements ActionListener
 	    txtAreaDisplayStats.setEditable(false);
 	    JScrollPane scrollPane = new JScrollPane(txtAreaDisplayStats);
 	    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setPreferredSize(new Dimension(400, 675));
+            scrollPane.setPreferredSize(new Dimension(400, 675));
 	 
-	    //statsPanel.add(scrollPane);
 	    BasePanel.add(btnGenerateStats);
 	    BasePanel.add(scrollPane);
 	    add(BasePanel);
 	}
 
-    private void createCrimeTree()
-    {
+    private void createCrimeTree() {
     	Random rand = new Random();
     	int iNumCities = rand.nextInt(6) + 2;
     	int[] iArySuburbs = new int[iNumCities];
     	
-    	for(int i = 0; i < iNumCities; i++)
-		{
-			iArySuburbs[i] = rand.nextInt(CrimeData.CRIMES_COMMITED) + 3; //Generate a random number of crimes per suburb
-		}
-    	
+    	for(int i = 0; i < iNumCities; i++) {
+		iArySuburbs[i] = rand.nextInt(CrimeData.CRIMES_COMMITED) + 3;
+	}
     	crimeTreeObj = new CrimeTree("Gauteng", iNumCities, iArySuburbs);
     }
 	
-	@Override
-	public void actionPerformed(ActionEvent event)
-	{
-		if(event.getSource() == btnGenerateStats)
-		{
+        @Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource() == btnGenerateStats) {
 			createCrimeTree();
-			txtAreaDisplayStats.setText(""); //Refresh the information displayed after each click
-			crimeTreeObj.populateTree(); //Populate the empty tree with random data
-			crimeTreeObj.crunchCrimeStats(); //Calculate and set the statistics
-			crimeTreeObj.displayTreeContents(txtAreaDisplayStats); //Display the 
+			txtAreaDisplayStats.setText(""); 
+			crimeTreeObj.populateTree(); 
+			crimeTreeObj.crunchCrimeStats(); 
+			crimeTreeObj.displayTreeContents(txtAreaDisplayStats); 
 		}
 	}
-
 }
